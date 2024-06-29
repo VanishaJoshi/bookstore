b9c5ebdbb19797923f52cf4d79eaf3cccaef616e
vanisha <vanishajoshi4741stella@gmail.com>
2024-06-29 12:36:17 +0530

    my first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Show from './Show'
+import Search from './Search'
+const App = () => {
+  return (
+    <div>
+        <Show />
+        <Search />
+        
+    </div>
+  )
+}
+
+export default App
